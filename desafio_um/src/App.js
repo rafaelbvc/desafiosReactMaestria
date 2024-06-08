9b08966728fffbed7f0d04a0d4c1225dbc9db7dd
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-07 21:13:55 -0300

    Desafios React do Zero a Maestria

---
+++ desafio_um/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Elemento da minha escolha, atribuido uma regra css */}
+      <button className="minha_escolha_btn">
+        <span className="text_btn">Elemento HTML da minha escolha.</span>
+      </button>
+    </div>
+  );
+}
+
+export default App;
