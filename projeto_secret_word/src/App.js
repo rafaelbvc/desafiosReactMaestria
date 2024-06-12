f047201f59d3ce94b610af00210827ac6f74be18
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-12 03:27:30 -0300

    projeto_secret_word

---
+++ projeto_secret_word/src/App.js
@@ -0,0 +1,26 @@
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import { wordsList } from "./data/words";
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+
+  return (
+    <div className="App">
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
+    </div>
+  );
+}
+
+export default App;
