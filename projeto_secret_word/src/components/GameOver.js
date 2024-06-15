97929a04f3cbe986b7dad728030c98468ab917b1
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-15 15:54:22 -0300

    Tela do Fim de Jogo

--- projeto_secret_word/src/components/GameOver.js
+++ projeto_secret_word/src/components/GameOver.js
@@ -1,9 +1,9 @@
 import "./GameOver.css";
 
-const GameOver = ({retry}) => {
+const GameOver = ({retry, score}) => {
   return (
     <div>
-      <h1>Game Over</h1>
+      <h1>Fim do jogo!</h1>
       <button onClick={retry}>Resetar jogo</button>
     </div>
   );
