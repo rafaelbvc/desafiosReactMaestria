84b939997d77c75bb5fca8ea533620deccefc4b1
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-17 01:49:51 -0300

    Custom Hook Get

--- desafio_seis/src/App.js
+++ desafio_seis/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useFetch } from "./hooks/useFetch";
 import "./App.css";
 
 const url = "http://localhost:3000/products";
@@ -6,20 +7,12 @@ const url = "http://localhost:3000/products";
 function App() {
   const [products, setProducts] = useState([]);
 
+  const { data: items } = useFetch(url);
+
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(url);
-
-      const data = await res.json();
-
-      setProducts(data);
-    }
-
-    fetchData();
-  }, []);
+  console.log(items);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -48,11 +41,12 @@ function App() {
       <h1>Lista de Produtos</h1>
       <br />
       <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.name} - R$: {product.price}
-          </li>
-        ))}
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+            </li>
+          ))}
       </ul>
       <div className="add-product">
         <form onSubmit={handleSubmit}>
