c963ed8427edb83574598a53b862e6e846191486
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-21 08:21:10 -0300

    Alterando contexto

--- desafio_oito/src/pages/Description.js
+++ desafio_oito/src/pages/Description.js
@@ -1,9 +1,15 @@
 import "./Description.css";
+import { useContext, useState } from "react";
+import { CounterContext } from "../context/CounterContext";
 
 const Description = () => {
+  const { counter, setCounter } = useContext(CounterContext);
+
   return (
     <div className="description_pg">
       <h3>Description</h3>
+      <p>Valor do contador: {counter}</p>
+      <button onClick={() => {setCounter(counter + 8)}}>ADD 8</button>
     </div>
   );
 };
--- desafio_oito/src/pages/Home.js
+++ desafio_oito/src/pages/Home.js
@@ -1,9 +1,17 @@
 import "./Home.css";
+import { useContext, useState } from "react";
+import { CounterContext } from "../context/CounterContext";
+import ChangeCounter from "../Components/ChangeCounter";
 
 const Home = () => {
+  const { counter } = useContext(CounterContext);
+
+
   return (
     <div className="home_pg">
       <h3>Home</h3>
+      <p>Valor do contador: {counter}</p>
+      <ChangeCounter />
     </div>
   );
 };
