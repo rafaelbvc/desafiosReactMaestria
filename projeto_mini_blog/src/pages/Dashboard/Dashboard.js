f4c6da210199bf4bfaa658262c8930e263641569
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-27 16:04:36 -0300

    Estrutura da Dashboard

--- projeto_mini_blog/src/pages/Dashboard/Dashboard.js
+++ projeto_mini_blog/src/pages/Dashboard/Dashboard.js
@@ -9,6 +9,8 @@ const Dashboard = () => {
 
   const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
 
+  const deleteDocument = (id) => {};
+
   return (
     <div>
       <h2>Dashboard</h2>
@@ -21,12 +23,37 @@ const Dashboard = () => {
           </Link>
         </div>
       ) : (
-        <div>
-          <p>Tem posts!</p>
-        </div>
+        <>
+          <div>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+          {Array.isArray(posts) &&
+            posts.map((post, index) => (
+              <div key={index}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                    Ver
+                  </Link>
+                  <Link
+                    to={`/posts/edit/${post.id}`}
+                    className="btn btn-outline"
+                  >
+                    Editar
+                  </Link>
+                  <button
+                    onChange={() => deleteDocument(post.id)}
+                    className="btn btn-outline btn-danger"
+                  >
+                    Excluir
+                  </button>
+                </div>
+              </div>
+            ))}
+        </>
       )}
       {loading && <p>Carregando...</p>}
-      {posts && posts.map((post, index) => <h3 key={index}>{post.title}</h3>)}
     </div>
   );
 };
