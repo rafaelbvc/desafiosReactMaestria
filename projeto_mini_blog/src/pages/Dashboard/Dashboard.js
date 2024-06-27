01548f1038e89f0c425da3f262047a496c883b6f
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-27 14:02:40 -0300

    Estrutura do DashBoard

--- projeto_mini_blog/src/pages/Dashboard/Dashboard.js
+++ projeto_mini_blog/src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,30 @@
-import styles from "./Dashboard.module.css"
+import styles from "./Dashboard.module.css";
+import { Link } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContex";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  const posts = [];
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Gerencie seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+        </div>   
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
