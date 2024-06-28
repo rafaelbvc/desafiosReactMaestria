ad3e6d8e5ecb3acd31b7df773ee08dfdca8a9a8b
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-28 20:55:03 -0300

    Controle Inputs useState

--- desafio_hooks/src/components/Hooks/HookUseState.js
+++ desafio_hooks/src/components/Hooks/HookUseState.js
@@ -5,12 +5,20 @@ const HookUseState = () => {
 
   const [name, setName] = useState("Matheus");
 
-
   const changeNames = () => {
-    userName = "João Souza" 
+    userName = "João Souza";
 
-    setName("Rafael Vendramini")
-  }
+    setName("Rafael Vendramini");
+  };
+
+  ///////////// controled inputs
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(age);
+  };
 
   return (
     <div>
@@ -18,6 +26,17 @@ const HookUseState = () => {
       <p>Variável: {userName}</p>
       <p>useState: {name}</p>
       <button onClick={changeNames}>Mudar Nomes!</button>
+      {/* /////////// controled inputs*/}
+      <p>Digite a sua idade:</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input type="submit" value="Enviar"/>
+      </form>
+      <p>Você tem {age} anos!</p>
     </div>
   );
 };
