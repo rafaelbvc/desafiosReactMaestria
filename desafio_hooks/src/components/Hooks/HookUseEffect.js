442b5ea7786f5b511081c3593cf0c7c52cec81d6
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-29 01:54:25 -0300

    useEffect Cleanup

--- desafio_hooks/src/components/Hooks/HookUseEffect.js
+++ desafio_hooks/src/components/Hooks/HookUseEffect.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const HookUseEffect = () => {
   // useEffect sem dependencia
   useEffect(() => {
-    console.log("estou sendo executado, dependencia vazia");
+    console.log("estou sendo executado!");
   });
 
   const [number, setNumber] = useState(1);
@@ -20,8 +20,23 @@ const HookUseEffect = () => {
 
   useEffect(() => {
     if (numberB > 0) {
-      console.log("estou sendo executado dependencia numberB, quando ele muda o state");
+      console.log(
+        "estou sendo executado dependencia numberB, quando ele muda o state"
+      );
     }
+  }, [numberB]);
+
+  ///cleanup do useEffect
+
+  useEffect(() => {
+    // const timer = setTimeout(() => {
+    //   console.log("2000ms");
+    // //   setNumberB(numberB + 1)
+    // }, 2000);
+
+    
+    //     //cleanup
+    //   return () => clearTimeout(timer)
   }, [numberB]);
 
   return (
