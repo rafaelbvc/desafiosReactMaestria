c9776e68c52ed6ba25962239b94de1ad98c99c8b
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-29 02:05:28 -0300

    HookUseContext

--- desafio_hooks/src/pages/About/About.js
+++ desafio_hooks/src/pages/About/About.js
@@ -1,9 +1,14 @@
+import { SomeContext } from "../../components/Hooks/HookUseContext";
 import styles from "./About.module.css";
+import { useContext } from "react";
 
 const About = () => {
+  const { contextValue } = useContext(SomeContext);
+
   return (
     <div className={styles.about_container}>
       <h3>About</h3>
+      <p>Valor do context: {contextValue}</p>
     </div>
   );
 };
--- desafio_hooks/src/pages/Home/Home.js
+++ desafio_hooks/src/pages/Home/Home.js
@@ -1,15 +1,21 @@
+import { SomeContext } from "../../components/Hooks/HookUseContext";
 import HookUseEffect from "../../components/Hooks/HookUseEffect";
 import HookUseReducer from "../../components/Hooks/HookUseReducer";
 import HookUseState from "../../components/Hooks/HookUseState";
 import styles from "./Home.module.css";
+import { useContext } from "react";
 
 const Home = () => {
+  const { contextValue } = useContext(SomeContext);
+
   return (
     <div className={styles.home_container}>
       <h1>Home</h1>
       <HookUseState />
       <HookUseReducer />
       <HookUseEffect />
+      <h2>useContext</h2>
+      <p>Valor do context: {contextValue}</p>
     </div>
   );
 };
