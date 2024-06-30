a3c5b5bae5f2edca39ca2fa066ceb9f7e11e66eb
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-30 00:05:01 -0300

    useDebugValue

--- desafio_hooks/src/components/Hooks/HooKCustomHook/HookUseCustom.js
+++ desafio_hooks/src/components/Hooks/HooKCustomHook/HookUseCustom.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { useCustomHook } from "./useCustomHook";
+import { useHookDebugValue } from "../HookUseDebugValue";
 
 const HookUseCustom = () => {
   const [number, setNumber] = useState(0);
   const previousValue = useCustomHook(number);
+
+  const  ref  = useHookDebugValue(number);
 
   return (
     <div>
@@ -11,6 +14,7 @@ const HookUseCustom = () => {
       <p>Autal: {number}</p>
       <p>Anterior: {previousValue}</p>
       <button onClick={() => setNumber(number + 1)}>Alterar!</button>
+      <p>From Debug: {ref}</p>
       <hr />
     </div>
   );
