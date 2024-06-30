b6aa7000bf092ca121c5fdab9c0eb76467283a42
rafaelbvc <rafaelbvc@hotmail.com>
2024-06-30 08:18:52 -0300

    Connection with DB

--- projeto_react_gram/projeto_react_gram_backend/config/db.js
+++ projeto_react_gram/projeto_react_gram_backend/config/db.js
@@ -1,1 +1,18 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+const connect = async () => {
+  try {
+    const dbcConnect = await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.h6zj5rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("Conectado ao banco!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connect();
+
+module.exports = connect;
